1faa8fb9bcda24938b2a0aaa1334b2e60907f959
oyvindBjerke <o.t.bjerke@gmail.com>
2015-03-25 18:06:01 +0100

    Updated UserRepository

--- src/main/kotlin/no/ciber/repository/UserRepository.kt
+++ src/main/kotlin/no/ciber/repository/UserRepository.kt
@@ -3,16 +3,15 @@ package no.ciber.repository
 import no.ciber.domain.User
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.repository.query.Param
-import org.springframework.data.rest.core.annotation.RestResource
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
 /**
  * Created by bjerke.
  */
-RestResource(rel = "user")
-trait UserRepository : PagingAndSortingRepository<User, java.lang.String> {
-    fun findByLastname(Param("name") name: String, pageable: Pageable): Page<User>
-    fun findByFirstname(Param("name") name: String, pageable: Pageable): Page<User>
-    fun findByEmail(Param("email") email: String, pageable: Pageable): Page<User>
+RepositoryRestResource(path = "/users")
+trait UserRepository : PagingAndSortingRepository<User, java.lang.String>, JpaRepository<User, java.lang.String> {
+    fun findByEmail(Param("email") email: String?): User?
 }
