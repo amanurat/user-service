6983d3e3602836c363ac871729e955c53c1ed2bc
oyvindBjerke <o.t.bjerke@gmail.com>
2015-03-25 18:09:09 +0100

    Removed unused import

--- src/main/kotlin/no/ciber/ControllerAdvice/UserControllerAdvice.kt
+++ src/main/kotlin/no/ciber/ControllerAdvice/UserControllerAdvice.kt
@@ -1,7 +1,6 @@
 package no.ciber.ControllerAdvice
 
 import no.ciber.Exception.DuplicateEmailException
-import no.ciber.validator.EmailValidator
 import org.apache.commons.logging.Log
 import org.apache.commons.logging.LogFactory
 import org.springframework.http.HttpStatus
