9f8dc16330dfee6279609d219a95f973b558560b
oyvindBjerke <o.t.bjerke@gmail.com>
2015-03-27 13:57:09 +0100

    Added binding of system variables to system properties

--- src/main/kotlin/no/ciber/Application.kt
+++ src/main/kotlin/no/ciber/Application.kt
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration
+import org.springframework.util.StringUtils
 import kotlin.platform.platformStatic
 
 SpringBootApplication
@@ -26,5 +27,16 @@ public open class Application : RepositoryRestMvcConfiguration() {
 }
 
 public fun main(args: Array<String>) {
+    setPropertyFromSystemVariable("DB_URL", "spring.datasource.url")
+    setPropertyFromSystemVariable("DB_USER", "spring.datasource.username")
+    setPropertyFromSystemVariable("DB_PW", "spring.datasource.password")
+    setPropertyFromSystemVariable("DB_DDL", "spring.jpa.hibernate.ddl-auto")
     Application.main(args)
+}
+
+private fun setPropertyFromSystemVariable(systemVariable: String?, propertyName: String?) {
+    var variable = System.getenv(systemVariable)
+    if (!StringUtils.isEmpty(variable)) {
+        System.setProperty(propertyName, variable)
+    }
 }
