66f5c2be7f84acb2fde7e2e46bbf1156c9140268
oyvindBjerke <o.t.bjerke@gmail.com>
2015-04-15 16:29:10 +0200

    Implemented search functionality for specific ids

--- src/main/java/no/ciber/repository/UserRepository.java
+++ src/main/java/no/ciber/repository/UserRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 /**
  * Created by bjerke.
  */
 @RepositoryRestResource(path = "/users")
 public interface UserRepository extends PagingAndSortingRepository<AppUser, Integer>, JpaRepository<AppUser, Integer>{
     AppUser findByEmail(@Param("email") String email);
+    List<AppUser> findByIdIn(@Param("ids") List<Integer> ids);
 }
